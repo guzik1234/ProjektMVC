b29a82bb1b533efa20f8499870d234f6fe09eba3
Krzysztof <krzyscar@gmail.com>
2025-11-19 16:49:01 +0100

    zmiana testowa

--- obj/Debug/net8.0/Projekt_MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projekt_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7592934475238b99958d731cb906001dfd55592e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53aa9101e40ca2b163838bee789872651baeea89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
